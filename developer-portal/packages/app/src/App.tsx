57d98e7f004a87819689bb2f5829be7f645ac520
SRE / DevOps <43549868+westibcar@users.noreply.github.com>
2026-07-05 12:22:09 -0300

    adicionando projetos

--- developer-portal/packages/app/src/App.tsx
+++ developer-portal/packages/app/src/App.tsx
@@ -5,7 +5,36 @@ import catalogPlugin from '@backstage/plugin-catalog/alpha';
 // mostrando pods, deployments, services e outros recursos do cluster em tempo real.
 import kubernetesPlugin from '@backstage/plugin-kubernetes/alpha';
 import { navModule } from './modules/nav';
+import { githubAuthApiRef } from '@backstage/core-plugin-api';
+import { SignInPageBlueprint } from '@backstage/plugin-app-react';
+import { SignInPage } from '@backstage/core-components';
+import { createFrontendModule } from '@backstage/frontend-plugin-api';
+
+const signInPage = SignInPageBlueprint.make({
+  params: {
+    loader: async () => props =>
+      (
+        <SignInPage
+          {...props}
+          provider={{
+            id: 'github-auth-provider',
+            title: 'GitHub',
+            message: 'Sign in using GitHub',
+            apiRef: githubAuthApiRef,
+          }}
+        />
+      ),
+  },
+});
 
 export default createApp({
-  features: [catalogPlugin, kubernetesPlugin, navModule],
+  features: [
+    catalogPlugin, 
+    kubernetesPlugin, 
+    navModule,
+    createFrontendModule({
+      pluginId: 'app',
+      extensions: [signInPage],
+    }),
+  ],
 });
--- developer-portal/packages/backend/src/index.ts
+++ developer-portal/packages/backend/src/index.ts
@@ -29,6 +29,9 @@ backend.add(import('@backstage/plugin-auth-backend'));
 // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 // See https://backstage.io/docs/auth/guest/provider
+// autenticacao com Github
+backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
+
 
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend'));
